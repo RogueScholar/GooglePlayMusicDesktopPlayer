6aaad68fb44942002c5efcb5f2520fa3b8008ec9
Restyled.io <commits@restyled.io>
2019-11-25 03:50:32 +0000

    Restyled by prettier

--- src/main/features/linux/mprisService.js
+++ src/main/features/linux/mprisService.js
@@ -1,94 +1,100 @@
-import {app} from 'electron';
-import mpris from 'mpris-service';
+import { app } from "electron";
+import mpris from "mpris-service";
 
 function mprisService() {
-  const mainWindow = WindowManager.getAll('main')[0];
+  const mainWindow = WindowManager.getAll("main")[0];
 
   const player = mpris({
-    name : 'google_play_music_desktop_player',
-    identity : 'Google Play Music Desktop Player',
-    canRaise : true,
-    supportedUriSchemes : [ 'https' ],
-    supportedMimeTypes : [ 'audio/mpeg' ],
-    supportedInterfaces : [ 'player' ],
-    desktopEntry : 'google-play-music-desktop-player',
+    name: "google_play_music_desktop_player",
+    identity: "Google Play Music Desktop Player",
+    canRaise: true,
+    supportedUriSchemes: ["https"],
+    supportedMimeTypes: ["audio/mpeg"],
+    supportedInterfaces: ["player"],
+    desktopEntry: "google-play-music-desktop-player"
   });
 
-  player.playbackStatus = 'Stopped';
+  player.playbackStatus = "Stopped";
   player.canEditTracks = false;
 
-  player.getPosition = function
-  getPosition() { return PlaybackAPI.currentTime().current * 1e3; };
+  player.getPosition = function getPosition() {
+    return PlaybackAPI.currentTime().current * 1e3;
+  };
 
-  player.on('raise', () => {
+  player.on("raise", () => {
     mainWindow.setSkipTaskbar(false);
     mainWindow.show();
   });
 
-  player.on('quit', () => { app.quit(); });
+  player.on("quit", () => {
+    app.quit();
+  });
 
-  player.on('play', () => {
+  player.on("play", () => {
     if (!PlaybackAPI.isPlaying()) {
-      Emitter.sendToGooglePlayMusic('playback:playPause');
+      Emitter.sendToGooglePlayMusic("playback:playPause");
     }
   });
 
-  player.on('pause', () => {
+  player.on("pause", () => {
     if (PlaybackAPI.isPlaying()) {
-      Emitter.sendToGooglePlayMusic('playback:playPause');
+      Emitter.sendToGooglePlayMusic("playback:playPause");
     }
   });
 
-  player.on('playpause',
-            () => { Emitter.sendToGooglePlayMusic('playback:playPause'); });
-
-  player.on('next',
-            () => { Emitter.sendToGooglePlayMusic('playback:nextTrack'); });
-
-  player.on('previous',
-            () => { Emitter.sendToGooglePlayMusic('playback:previousTrack'); });
-
-  player.on('stop', () => {
-    Emitter.sendToGooglePlayMusic('playback:stop');
-    player.playbackStatus = 'Stopped';
+  player.on("playpause", () => {
+    Emitter.sendToGooglePlayMusic("playback:playPause");
   });
 
-  player.on('volume', (volume) => {
-    Emitter.sendToGooglePlayMusic('execute:gmusic', {
-      namespace : 'volume',
-      method : 'setVolume',
-      args : [ Math.round(volume * 100) ],
+  player.on("next", () => {
+    Emitter.sendToGooglePlayMusic("playback:nextTrack");
+  });
+
+  player.on("previous", () => {
+    Emitter.sendToGooglePlayMusic("playback:previousTrack");
+  });
+
+  player.on("stop", () => {
+    Emitter.sendToGooglePlayMusic("playback:stop");
+    player.playbackStatus = "Stopped";
+  });
+
+  player.on("volume", volume => {
+    Emitter.sendToGooglePlayMusic("execute:gmusic", {
+      namespace: "volume",
+      method: "setVolume",
+      args: [Math.round(volume * 100)]
     });
   });
 
-  player.on('position', (data) => {
-    Emitter.sendToGooglePlayMusic('playback:seek', data.position / 1e3);
+  player.on("position", data => {
+    Emitter.sendToGooglePlayMusic("playback:seek", data.position / 1e3);
   });
 
-  player.on('seek', (offset) => {
+  player.on("seek", offset => {
     const currentTime = PlaybackAPI.currentTime().current;
-    const requestedTime = currentTime + (offset / 1e3);
-    Emitter.sendToGooglePlayMusic('playback:seek', requestedTime);
+    const requestedTime = currentTime + offset / 1e3;
+    Emitter.sendToGooglePlayMusic("playback:seek", requestedTime);
   });
 
-  PlaybackAPI.on('change:track', (newSong) => {
+  PlaybackAPI.on("change:track", newSong => {
     player.canSeek = true;
     player.canPlay = true;
     player.canPause = true;
     player.canGoPrevious = true;
     player.canGoNext = true;
     player.metadata = {
-      'mpris:artUrl' : newSong.albumArt.replace('=s90', '=s300'),
-      'mpris:length' : PlaybackAPI.currentTime().total * 1e3,
-      'xesam:asText' : (newSong.lyrics || ''),
-      'xesam:title' : newSong.title,
-      'xesam:album' : newSong.album,
-      'xesam:artist' : [ newSong.artist ],
+      "mpris:artUrl": newSong.albumArt.replace("=s90", "=s300"),
+      "mpris:length": PlaybackAPI.currentTime().total * 1e3,
+      "xesam:asText": newSong.lyrics || "",
+      "xesam:title": newSong.title,
+      "xesam:album": newSong.album,
+      "xesam:artist": [newSong.artist]
     };
   });
 
   let lastPosition = 0;
-  PlaybackAPI.on('change:time', (time) => {
+  PlaybackAPI.on("change:time", time => {
     const newPosition = time.current * 1e3;
 
     if (Math.abs(lastPosition - newPosition) > 2e6) {
@@ -100,14 +106,15 @@ function mprisService() {
     lastPosition = newPosition;
   });
 
-  PlaybackAPI.on('change:state', (playbackState) => {
+  PlaybackAPI.on("change:state", playbackState => {
     // DEV: We skip stopped here because PlaybackAPI only shuttles true/false
     // from GPM
-    player.playbackStatus = (playbackState) ? 'Playing' : 'Paused';
+    player.playbackStatus = playbackState ? "Playing" : "Paused";
   });
 
-  PlaybackAPI.on('change:volume',
-                 (volume) => { player.volume = volume / 100; });
+  PlaybackAPI.on("change:volume", volume => {
+    player.volume = volume / 100;
+  });
 }
 
 mprisService();
