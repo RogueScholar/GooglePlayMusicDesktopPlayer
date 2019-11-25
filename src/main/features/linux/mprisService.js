c48da3b55e78051084faf7b02c4954230c4b07dd
Restyled.io <commits@restyled.io>
2019-11-25 03:50:31 +0000

    Restyled by clang-format

--- src/main/features/linux/mprisService.js
+++ src/main/features/linux/mprisService.js
@@ -1,34 +1,31 @@
-import { app } from 'electron';
+import {app} from 'electron';
 import mpris from 'mpris-service';
 
 function mprisService() {
   const mainWindow = WindowManager.getAll('main')[0];
 
   const player = mpris({
-    name: 'google_play_music_desktop_player',
-    identity: 'Google Play Music Desktop Player',
-    canRaise: true,
-    supportedUriSchemes: ['https'],
-    supportedMimeTypes: ['audio/mpeg'],
-    supportedInterfaces: ['player'],
-    desktopEntry: 'google-play-music-desktop-player',
+    name : 'google_play_music_desktop_player',
+    identity : 'Google Play Music Desktop Player',
+    canRaise : true,
+    supportedUriSchemes : [ 'https' ],
+    supportedMimeTypes : [ 'audio/mpeg' ],
+    supportedInterfaces : [ 'player' ],
+    desktopEntry : 'google-play-music-desktop-player',
   });
 
   player.playbackStatus = 'Stopped';
   player.canEditTracks = false;
 
-  player.getPosition = function getPosition() {
-    return PlaybackAPI.currentTime().current * 1e3;
-  };
+  player.getPosition = function
+  getPosition() { return PlaybackAPI.currentTime().current * 1e3; };
 
   player.on('raise', () => {
     mainWindow.setSkipTaskbar(false);
     mainWindow.show();
   });
 
-  player.on('quit', () => {
-    app.quit();
-  });
+  player.on('quit', () => { app.quit(); });
 
   player.on('play', () => {
     if (!PlaybackAPI.isPlaying()) {
@@ -42,17 +39,14 @@ function mprisService() {
     }
   });
 
-  player.on('playpause', () => {
-    Emitter.sendToGooglePlayMusic('playback:playPause');
-  });
+  player.on('playpause',
+            () => { Emitter.sendToGooglePlayMusic('playback:playPause'); });
 
-  player.on('next', () => {
-    Emitter.sendToGooglePlayMusic('playback:nextTrack');
-  });
+  player.on('next',
+            () => { Emitter.sendToGooglePlayMusic('playback:nextTrack'); });
 
-  player.on('previous', () => {
-    Emitter.sendToGooglePlayMusic('playback:previousTrack');
-  });
+  player.on('previous',
+            () => { Emitter.sendToGooglePlayMusic('playback:previousTrack'); });
 
   player.on('stop', () => {
     Emitter.sendToGooglePlayMusic('playback:stop');
@@ -61,9 +55,9 @@ function mprisService() {
 
   player.on('volume', (volume) => {
     Emitter.sendToGooglePlayMusic('execute:gmusic', {
-      namespace: 'volume',
-      method: 'setVolume',
-      args: [Math.round(volume * 100)],
+      namespace : 'volume',
+      method : 'setVolume',
+      args : [ Math.round(volume * 100) ],
     });
   });
 
@@ -84,15 +78,14 @@ function mprisService() {
     player.canGoPrevious = true;
     player.canGoNext = true;
     player.metadata = {
-      'mpris:artUrl': newSong.albumArt.replace('=s90', '=s300'),
-      'mpris:length': PlaybackAPI.currentTime().total * 1e3,
-      'xesam:asText': (newSong.lyrics || ''),
-      'xesam:title': newSong.title,
-      'xesam:album': newSong.album,
-      'xesam:artist': [newSong.artist],
+      'mpris:artUrl' : newSong.albumArt.replace('=s90', '=s300'),
+      'mpris:length' : PlaybackAPI.currentTime().total * 1e3,
+      'xesam:asText' : (newSong.lyrics || ''),
+      'xesam:title' : newSong.title,
+      'xesam:album' : newSong.album,
+      'xesam:artist' : [ newSong.artist ],
     };
   });
-
 
   let lastPosition = 0;
   PlaybackAPI.on('change:time', (time) => {
@@ -108,13 +101,13 @@ function mprisService() {
   });
 
   PlaybackAPI.on('change:state', (playbackState) => {
-    // DEV: We skip stopped here because PlaybackAPI only shuttles true/false from GPM
+    // DEV: We skip stopped here because PlaybackAPI only shuttles true/false
+    // from GPM
     player.playbackStatus = (playbackState) ? 'Playing' : 'Paused';
   });
 
-  PlaybackAPI.on('change:volume', (volume) => {
-    player.volume = volume / 100;
-  });
+  PlaybackAPI.on('change:volume',
+                 (volume) => { player.volume = volume / 100; });
 }
 
 mprisService();
